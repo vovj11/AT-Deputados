fa45fd117e85ea296204fe77e336a612cfffa9b1
vovj11 <vovj11@hotmail.com>
2022-10-05 18:41:59 -0300

    Consertado erro de scroll onde ele pulava ao passar da imagem

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 (function () {
   var menu = document.getElementById("menu-inicio"); // colocar em cache
+  var inicio = document.getElementById("inicio");
+  var tamInicio = inicio.scrollHeight;
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 780)
+    if (window.scrollY > tamInicio)
       menu.classList.add("menu-ativo"); // > 0 ou outro valor desejado
     else menu.classList.remove("menu-ativo");
   });
